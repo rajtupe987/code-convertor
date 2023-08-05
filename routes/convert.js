c8bbd2abb9fb7df5da0458517fd40b40508e0952
Raj Tupe <114012248+rajtupe987@users.noreply.github.com>
2023-08-05 13:22:16 +0530

    done

--- routes/convert.js
+++ routes/convert.js
@@ -12,12 +12,10 @@ code_convertor.post('/convert', async (req, res) => {
 
     const code=req.body.code;
     const targetLanguage=req.body.targetLanguage
- 
-     const issues = analyzeCodeForMissingSemicolons(code);
-     const codeQuality = assessCodeQuality(code);
+
     // Validate if the target language is supported
     if (!supportedLanguages.includes(targetLanguage)) {
-      console.log("Unsupported target language:", targetLanguage);
+      //console.log("Unsupported target language:", targetLanguage);
       return res.status(400).json({ error: "Unsupported target language." });
     }
   
@@ -39,7 +37,7 @@ code_convertor.post('/convert', async (req, res) => {
   
       const convertedCode = response.data.choices[0].text;
       //console.log(convertedCode)
-      res.json({ "convertedCode": convertedCode,issues,codeQuality });
+      res.json({ "convertedCode": convertedCode});
     } catch (error) {
       console.log(error.message);
       res.status(500).json({ error: error.message });
@@ -49,58 +47,81 @@ code_convertor.post('/convert', async (req, res) => {
  
 
 
-
-  // Code analysis function to check for missing semicolons in JavaScript code
-function analyzeCodeForMissingSemicolons(code) {
-  const lines = code.split('\n');
-  const issues = [];
-
-  lines.forEach((line, lineNumber) => {
-    if (line.trim() !== '' && !line.trim().endsWith(';')) {
-      issues.push({
-        type: 'MissingSemicolon',
-        line: lineNumber + 1,
-        message: 'Missing semicolon at the end of the line.'
-      });
-    }
-  });
-
-  return issues;
-}
+// for debug
+code_convertor.post('/debug', async (req, res) => {
+  
+  const code=req.body.code;
 
 
+  if (!code) {
+    //console.log("Unsupported target language:", targetLanguage);
+    return res.status(400).json({ error: "plese provide code" });
+  }
 
 
-///quality check of the code
-function assessCodeQuality(code) {
-  const codeQuality = {
-    complexity: calculateCyclomaticComplexity(code),
-    length: code.split('\n').length,
-    naming: checkNamingConventions(code),
-    duplication: checkCodeDuplication(code)
-    // Add more quality checks as needed
-  };
-
-  return codeQuality;
-}
-
-function calculateCyclomaticComplexity(code) {
-  const complexity = (code.match(/if|else|for|while|switch/g) || []).length;
-  return complexity;
-}
+  try {
+    // Create a prompt based on the selected target language and the provided code
+    const prompt = `debug this code and also give some suggetion that how you can improve your given code ${code}`;
 
 
-function checkNamingConventions(code) {
-  const variablesAndFunctions = code.match(/\b([a-z]+[A-Z]\w*)\b|\b([A-Z]+\w*[a-z])\b/g) || [];
-  const nonCamelCaseNames = variablesAndFunctions.filter(name => !(/[A-Z]/.test(name[0])));
-  return nonCamelCaseNames.length === 0;
-}
+    // Make a request to the GPT API for code conversion
+    const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+      prompt: prompt,
+      max_tokens: 2048,
+      temperature: 1,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${GPT_API_KEY}`,
+      },
+    });
+
+    const debugcode = response.data.choices[0].text;
+    //console.log(convertedCode)
+    res.json({ "debugedcode": debugcode});
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+
+});
+
+// Route for handling the accuracy button
+code_convertor.post('/accuracy', async (req, res) => {
+  
+  const code=req.body.code;
+
+  if (!code) {
+    //console.log("Unsupported target language:", targetLanguage);
+    return res.status(400).json({ error: "plese provide code" });
+  }
+
+  try {
+    // Create a prompt based on the selected target language and the provided code
+    const prompt = `check the accuracy of given code check every thing inside the provided code then give the accuracy of the givien code${code}`;
+
+    // Make a request to the GPT API for code conversion
+    const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+      prompt: prompt,
+      max_tokens: 2048,
+      temperature: 1,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${GPT_API_KEY}`,
+      },
+    });
+
+    const accuracy_code = response.data.choices[0].text;
+    //console.log(convertedCode)
+    res.json({ "accuratecode": accuracy_code});
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+
+});
 
 
-function checkCodeDuplication(code) {
-  const lines = code.split('\n');
-  const duplicatedLines = lines.filter((line, index) => lines.indexOf(line) !== index);
-  return duplicatedLines.length === 0;
-}
 
 module.exports=code_convertor;
